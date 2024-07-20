e23334612daad02b771198b0bab1ce741b27dbfa
Davi Sales Barreira <davisbarreira@gmail.com>
2024-07-20 15:44:53 -0300

    :white_check_mark: Adding test for encoding function.

--- test/encoding/test_quickplot.jl
+++ test/encoding/test_quickplot.jl
@@ -10,5 +10,18 @@ using StructArrays
         y=row -> row.x^2
     )
     @test string(draw(plt)) isa String
+
+
+    # Testing other variations
+    data = StructArray(x=range(0, 10, 20), y=range(0, 10, 20), z=rand(["a", "b"], 20))
+    plt = plot(
+        data,
+        x=(value=collect(range(0, 20, 20)), datatype=:q),
+        y=row -> row.x^2,
+        color=(field=:z,),
+        size=(value=rand(20),),
+    )
+    draw(plt)
+    @test string(draw(plt)) isa String
 end
 
