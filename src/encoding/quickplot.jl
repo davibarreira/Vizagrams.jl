502a1cc901df45962bdcc5ed4845fb512fefc10b
Davi Sales Barreira <davisbarreira@gmail.com>
2024-08-02 20:57:37 -0300

    :truck: plot in quickplot not needed anymore. It was joined to the Plot mark.

--- src/encoding/quickplot.jl
+++ src/encoding/quickplot.jl
@@ -15,127 +15,130 @@ plot(df,x=:island,y=:species)
 ```
 """
 function plot(
-    data;
+    data=nothing;
     graphic=Circle(; r=3),
-    style=S(),
     config=NamedTuple(),
     figsize=(300, 200),
     title="",
     kwargs...,
 )
-    config = NamedTupleTools.rec_merge(
-        (xaxis=(grid=(flag=true,),), yaxis=(grid=(flag=true,),)), config
-    )
-
-    data = StructArray(Tables.rowtable(data))
-    encodings = Dict()
-    for (k, v) in kwargs
-
-        # This is the case where the variable has only the data as in x=[1,2,4]
-        if isa(v, Symbol)
-            encodings[k] = Dict(:field => v)
-            continue
-        elseif v isa Function
-            fdata = StructArray(NamedTuple(Dict(k => map(v, data))))
-            data = hconcat(data, fdata, "_")
-            field = propertynames(data)[end]
-            encodings[k] = Dict(:field => field)
-            continue
-        elseif v isa Vector
-            fdata = StructArray(NamedTuple(Dict(k => v)))
-            data = hconcat(data, fdata, "_")
-            field = propertynames(data)[end]
-            encodings[k] = Dict(:field => field)
-            continue
-        end
-        encodings[k] = Dict(:field => k)
-
-        # This is the case where the user passes the data and more x=(data=[1,2,3],datatype=:n,...)
-        for (k_, v_) in pairs(v)
-            if k_ == :value
-                if v_ isa Vector
-                    fdata = StructArray(NamedTuple(Dict(k => v_)))
-                elseif v_ isa Function
-                    fdata = StructArray(NamedTuple(Dict(k => map(v_, data))))
-                end
-                data = hconcat(data, fdata, "_")
-                field = propertynames(data)[end]
-                encodings[k] = Dict(:field => field)
-                continue
-            end
-            encodings[k] = merge(encodings[k], Dict(k_ => v_))
-        end
-    end
-
     return Plot(;
-        figsize=figsize,
-        data=data,
-        title=title,
-        config=config,
-        encodings=encodings,
-        graphic=GraphicExpression(graphic),
+        data=data, graphic=graphic, config=config, figsize=figsize, title=title, kwargs...
     )
 end
+#     config = NamedTupleTools.rec_merge(
+#         (xaxis=(grid=(flag=true,),), yaxis=(grid=(flag=true,),)), config
+#     )
 
-"""
-plot(;
-    figsize=(300, 200),
-    title="",
-    config=NamedTuple(),
-    graphic=Circle(; r=3),
-    kwargs...,
-)
+#     data = StructArray(Tables.rowtable(data))
+#     encodings = Dict()
+#     for (k, v) in kwargs
 
-Provides a quicker way to specify a plot. For example:
-```julia
+#         # This is the case where the variable has only the data as in x=[1,2,4]
+#         if isa(v, Symbol)
+#             encodings[k] = Dict(:field => v)
+#             continue
+#         elseif v isa Function
+#             fdata = StructArray(NamedTuple(Dict(k => map(v, data))))
+#             data = hconcat(data, fdata, "_")
+#             field = propertynames(data)[end]
+#             encodings[k] = Dict(:field => field)
+#             continue
+#         elseif v isa Vector
+#             fdata = StructArray(NamedTuple(Dict(k => v)))
+#             data = hconcat(data, fdata, "_")
+#             field = propertynames(data)[end]
+#             encodings[k] = Dict(:field => field)
+#             continue
+#         end
+#         encodings[k] = Dict(:field => k)
 
-# simple lineplot
-p = plot(x=[1, 2, 3], y=[1, 2, 3], graphic=Line())
+#         # This is the case where the user passes the data and more x=(data=[1,2,3],datatype=:n,...)
+#         for (k_, v_) in pairs(v)
+#             if k_ == :value
+#                 if v_ isa Vector
+#                     fdata = StructArray(NamedTuple(Dict(k => v_)))
+#                 elseif v_ isa Function
+#                     fdata = StructArray(NamedTuple(Dict(k => map(v_, data))))
+#                 end
+#                 data = hconcat(data, fdata, "_")
+#                 field = propertynames(data)[end]
+#                 encodings[k] = Dict(:field => field)
+#                 continue
+#             end
+#             encodings[k] = merge(encodings[k], Dict(k_ => v_))
+#         end
+#     end
 
-# more complicated lineplot
-p = plot(;
-    x=(value=[1, 2, 3, 1], datatype=:q),
-    y=[1, 2, 2, 1],
-    color=(value=[1, 1, 2, 2], datatype=:n),
-    graphic=S(:strokeWidth => 10, :strokeOpacity => 0.9)Line(),
-)
-```
-"""
-function plot(;
-    figsize=(300, 200), title="", config=NamedTuple(), graphic=Circle(; r=3), kwargs...
-)
-    config = NamedTupleTools.rec_merge(
-        (xaxis=(grid=(flag=true,),), yaxis=(grid=(flag=true,),)), config
-    )
+#     return Plot(;
+#         figsize=figsize,
+#         data=data,
+#         title=title,
+#         config=config,
+#         encodings=encodings,
+#         graphic=GraphicExpression(graphic),
+#     )
+# end
 
-    encodings = Dict()
-    data = Dict()
-    for (k, v) in kwargs
-        # This is the case where the variable has only the data as in x=[1,2,4]
-        if v isa Vector
-            data[k] = v
-            encodings[k] = Dict(:field => k)
-            continue
-        end
+# """
+# plot(;
+#     figsize=(300, 200),
+#     title="",
+#     config=NamedTuple(),
+#     graphic=Circle(; r=3),
+#     kwargs...,
+# )
 
-        # This is the case where the user passes the data and more x=(data=[1,2,3],datatype=:n,...)
-        for (k_, v_) in pairs(v)
-            if k_ == :value
-                data[k] = v_
-                encodings[k] = Dict(:field => k)
-                continue
-            end
-            encodings[k] = merge(encodings[k], Dict(k_ => v_))
-        end
-    end
-    data = StructArray(NamedTuple(data))
+# Provides a quicker way to specify a plot. For example:
+# ```julia
 
-    return Plot(;
-        figsize=figsize,
-        data=data,
-        title=title,
-        config=config,
-        encodings=encodings,
-        graphic=GraphicExpression(graphic),
-    )
-end
+# # simple lineplot
+# p = plot(x=[1, 2, 3], y=[1, 2, 3], graphic=Line())
+
+# # more complicated lineplot
+# p = plot(;
+#     x=(value=[1, 2, 3, 1], datatype=:q),
+#     y=[1, 2, 2, 1],
+#     color=(value=[1, 1, 2, 2], datatype=:n),
+#     graphic=S(:strokeWidth => 10, :strokeOpacity => 0.9)Line(),
+# )
+# ```
+# """
+# function plot(;
+#     figsize=(300, 200), title="", config=NamedTuple(), graphic=Circle(; r=3), kwargs...
+# )
+#     config = NamedTupleTools.rec_merge(
+#         (xaxis=(grid=(flag=true,),), yaxis=(grid=(flag=true,),)), config
+#     )
+
+#     encodings = Dict()
+#     data = Dict()
+#     for (k, v) in kwargs
+#         # This is the case where the variable has only the data as in x=[1,2,4]
+#         if v isa Vector
+#             data[k] = v
+#             encodings[k] = Dict(:field => k)
+#             continue
+#         end
+
+#         # This is the case where the user passes the data and more x=(data=[1,2,3],datatype=:n,...)
+#         for (k_, v_) in pairs(v)
+#             if k_ == :value
+#                 data[k] = v_
+#                 encodings[k] = Dict(:field => k)
+#                 continue
+#             end
+#             encodings[k] = merge(encodings[k], Dict(k_ => v_))
+#         end
+#     end
+#     data = StructArray(NamedTuple(data))
+
+#     return Plot(;
+#         figsize=figsize,
+#         data=data,
+#         title=title,
+#         config=config,
+#         encodings=encodings,
+#         graphic=GraphicExpression(graphic),
+#     )
+# end
